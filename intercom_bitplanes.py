365f75b54159accbb93028a9ce75afa7083f331f
Gelrok <Toni.Schaarschmidt@hotmail.de>
2019-10-22 22:12:43 +0200

    Solution Issue 42 Version 1.4

--- intercom_bitplanes.py
+++ intercom_bitplanes.py
@@ -1,9 +1,21 @@
 # Adding a buffer.
 
+#Current Version 1.4 - status: working
+#Restriction:   -increase samples per chunk to 2048
+#               -samples per have to be multiply of 8
+#Version history
+
+#1.3 - Added code comments
+#1.2 - Cleaning up metods
+#1.1 - new structure for seperat channel sending
+#1.0 - implementation bitplane object
+
 import sounddevice as sd
 import numpy as np
 import struct
 from intercom_buffer import Intercom_buffer
+#Import Object Intercom
+from intercom import Intercom
 
 if __debug__:
     import sys
@@ -13,42 +25,63 @@ class Intercom_bitplanes(Intercom_buffer):
     def init(self, args):
         Intercom_buffer.init(self, args)
         self.bitplaneregister = np.zeros(self.chunks_to_buffer,dtype="int16")
-        self.packet_format="!hhh{}c".format(self.sample_per_chunk/8)
-
+        self.packet_format="!BBH{}B".format((self.samples_per_chunk//8)//self.number_of_channels)
+        #sys.stderr.write("\n\nFORMAT: {}".format(self.packet_format)); sys.stderr.flush()
     def run(self):
-        self.recorded_chunk_number = -1
+        self.recorded_chunk_number = 0
         self.played_chunk_number = 0
-        self.currentbit = 16
-        self.channelbuffer = np.zeros((self.samples_per_chunk, self.number_of_channels),dtype=np.int16)
-        
+
         def receive_and_buffer():
             message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
-            bitplanenumber, chunk_number, *bitplane = struct.unpack(self.packet_format, message)
 
-            msg=np.unpackbits(bitplane)
-            msgchannel=msg.astype(np.int16)
+            #Get elements of sent message
+            #bp_number = bitplane number of package
+            #channel_number = channel number of package
+            #chunk_number = chunk number of package
+            bp_number, channel_number,chunk_number, *bitplane = struct.unpack(self.packet_format, message)
 
-            msgout=msgchannel.reshape((self.samples_per_chunk))
+            #Get sent Bitplane list and but in converted numpy array
+            bp_channel_array=np.asarray(bitplane, dtype=np.uint8)
+
+            #Unpack bits
+            bp_channel_raw=np.unpackbits(bp_channel_array)
             
-            self._buffer[chunk_number % self.cells_in_buffer]=self._buffer[chunk_number % self.cells_in_buffer][nrchannel]+msgout
-            #self._buffer[chunk_number % self.cells_in_buffer] = np.asarray(chunk).reshape(self.frames_per_chunk, self.number_of_channels)
+            #Convert values to signed integer 16 BIT
+            bp_channel_received=bp_channel_raw.astype(np.int16)
+
+            #Binary compare channel in message buffer with received Bitplane
+            self._buffer[chunk_number % self.cells_in_buffer][:,channel_number]=self._buffer[chunk_number % self.cells_in_buffer][:,channel_number] | bp_channel_received << bp_number
+
+            #if bp_number==0:
+            #    sys.stderr.write("\nREC_MSG[{}]: {}".format(chunk_number,self._buffer[chunk_number % self.cells_in_buffer])); sys.stderr.flush()                        
+    
             return chunk_number
 
         def record_send_and_play(indata, outdata, frames, time, status):
 
-            msg=np.frombuffer(message, np.int16).reshape(self.frames_per_chunk, self.number_of_channels)
+            #Get message from sound card buffer
+            msg=np.frombuffer(indata, np.int16).reshape(self.frames_per_chunk, self.number_of_channels)
 
-            for i in range(self.number_of_channels):
-                self.channelbuffer[i]=msg[:,i]
-
-                for b in range(16)
-                    bitplane=self.channelbuffer[i] >> (15-b) & 1
-                    conversion=bitplane.astype(np.byte)
-                    bitpack=np.packbits(convert)
-
-                    message = struct.pack(self.packet_format, (15-b), self.recorded_chunk_number, *(bitpack))
-                    self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port)
+            #sys.stderr.write("\n\nSND_MSG[{}]: {}".format(self.recorded_chunk_number,msg)); sys.stderr.flush()
             
+            #Iterate channels
+            for i in range(self.number_of_channels):
+                #Iterate Bitplanes
+                for b in range(15,-1,-1):
+                    #Get bitplane "b" of channel "i"
+                    bitplane=msg[:,i] >> b & 1
+                    #Conversion bits to unsigned integer
+                    bitplane_raw=bitplane.astype(np.uint8)
+                    #Pack bits to packet
+                    bitpack=np.packbits(bitplane_raw)
+
+                    #Create message with structure
+                    message = struct.pack(self.packet_format, b,i, self.recorded_chunk_number, *(bitpack))
+
+                    #Send message
+                    self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+
+            #Increase chunk number
             self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER    
 
             chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
