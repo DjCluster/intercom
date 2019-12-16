c66b890dfdcd7b30072613e50f922a1f188d2791
Gelrok <Toni.Schaarschmidt@hotmail.de>
2019-12-16 11:36:28 +0100

    Reorganizing Folder and Extending Comments in file

--- development/intercom_congestion.py
+++ development/intercom_congestion.py
@@ -1,8 +1,9 @@
 # Exploiting binaural redundancy.
 
-#Current Version 1.4 - status: working
+#Current Version 1.5 - status: working
 #Version history
 
+#1.5 - Extending comments for description
 #1.4 - Implementación versión Mono
 #1.3 - Correción Error Congestión
 #1.2 - Comments review
@@ -22,20 +23,20 @@ class Intercom_binaural(Intercom_bitplanes):
   def init(self, args):
     Intercom_bitplanes.init(self, args)
 
-    #Initialize missed packets to 32
+    #Initialize missed packets to 32 on stereo (16 if mono)
     #We define the initial value of the control buffer (count the packets that have arrived)    
     self.missed_packets_rec=16*self.number_of_channels
     self.missed_packets_own=16*self.number_of_channels
     self.missed_current=16*self.number_of_channels
-    self.packages_to_sent=16*self.number_of_channels
+    self.packages_to_sent=16*self.number_of_channels #(32=we await 32 packets)
     
-    #Initialize Control buffer with 32 missed packets in each position.
+    #Initialize Control buffer with 32 on stereo (16 if mono) missed packets in each position.
     #If it is 32 means that no package has arrived
     #If it is 0 means that all packages have arrived
     self._buffer_control=np.full([self.chunks_to_buffer*2,2],16*self.number_of_channels,dtype=np.uint8)
 
-    #Add "B" number of packets send (congestion value)
-    #Add "B" number of missed packets to struct format
+    #Add "B" number of packets to send(awaited packages)
+    #Add "B" number of missed packets to struct format (congestion value)
     self.packet_format = f"!HBBB{self.frames_per_chunk//8}B"
 
     if self.number_of_channels == 2:
@@ -44,7 +45,7 @@ class Intercom_binaural(Intercom_bitplanes):
   def receive_and_buffer(self):
     message, source_address = self.receiving_sock.recvfrom(Intercom_bitplanes.MAX_MESSAGE_SIZE)
 
-    #Get missed packets of recipient (missed_packets_rec)
+    #Get data and missed packets and awaited packets of recipient (missed_packets_rec)
     chunk_number, bitplane_number, packages_to_receive, self.missed_packets_rec,*bitplane = struct.unpack(self.packet_format, message)
     bitplane = np.asarray(bitplane, dtype=np.uint8)
     bitplane = np.unpackbits(bitplane)
@@ -65,13 +66,13 @@ class Intercom_binaural(Intercom_bitplanes):
     #The absolute value is made to make it positive
     indata_abs = abs(np.copy(indata))
     
-    #It is replaced with the bitplane of the original sign
+    #sign plane is replaced with the bitplane of the original value
     package = sign_plane << 15 | indata_abs
    
-    #The first bitplane that is 1 after the sign is searched
+    #The maximum bitplane in use of all values that is 1 after the sign is searched (limit)
     valmax=np.max(indata_abs)
     limite=math.ceil(math.log2(valmax))
-    self.packages_to_sent=(limite+1)*self.number_of_channels
+    self.packages_to_sent=(limite+1)*self.number_of_channels #(add sign plane to limit)
 
     #Minimum 2 bitplane is always sent (the rest depending on the congestion)
     #Sending the first bitplane (BIT 15)
@@ -83,7 +84,7 @@ class Intercom_binaural(Intercom_bitplanes):
       message = struct.pack(self.packet_format, self.recorded_chunk_number, bitplane_number, self.packages_to_sent, self.missed_packets_own, *bitplane)
       self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
       
-    #Sending the next bitplane (first bitplane to 1 after the sign) (limit calculated above)
+    #Sending the next bitplane (max bitplane in use after the sign) (limit calculated above)
     for bitplane_number in range(self.number_of_channels*(limite)-1, (self.number_of_channels*(limite-1)-(self.number_of_channels)), -1):
       bitplane = (package[:, bitplane_number%self.number_of_channels] >> bitplane_number//self.number_of_channels) & 1
       bitplane = bitplane.astype(np.uint8)
@@ -114,10 +115,10 @@ class Intercom_binaural(Intercom_bitplanes):
     #We consider the limit for not obtaining a false congestion value
     self.missed_current=abs((16*self.number_of_channels)-(self._buffer_control[(self.played_chunk_number) % self.cells_in_buffer][1])-(self._buffer_control[(self.played_chunk_number) % self.cells_in_buffer][0]))    
     
-    #Calculation to reset congestion
+    #Calculation to optimize and recover congestion
     self.missed_packets_own = int(self.missed_packets_own*0.8 + self.missed_current*0.2)-1
 
-    ##When the congestion is 0, this value will be -1, so we reset to 0
+    #When the congestion is 0, this value will be -1, so we reset to 0
     if self.missed_packets_own<0:
       self.missed_packets_own=0
 
@@ -136,16 +137,15 @@ class Intercom_binaural(Intercom_bitplanes):
     #Write the value in the playback buffer
     self._buffer[ self.played_chunk_number % self.cells_in_buffer ]=recovered_chunk
 
-    #Lookup missed packages of current buffer slot
+    #Recover Binaural channel
     self._buffer[self.played_chunk_number % self.cells_in_buffer][:,0] += self._buffer[self.played_chunk_number % self.cells_in_buffer][:,1]
 
     #Set missed packages for next round for current buffer slot
-
     #Reset the buffer_control for the current value
     self._buffer_control[ self.played_chunk_number % self.cells_in_buffer ][0] = 16*self.number_of_channels
     self._buffer_control[ self.played_chunk_number % self.cells_in_buffer ][1] = 16*self.number_of_channels
 
-    #Reduce own congestions exponentially
+    #Reproduce sound data
     self.play(outdata)
 
 def record_send_and_play(self, indata, outdata, frames, time, status):
@@ -182,7 +182,7 @@ def record_send_and_play(self, indata, outdata, frames, time, status):
     self._buffer_control[ self.played_chunk_number % self.cells_in_buffer ][0] = 16*self.number_of_channels
     self._buffer_control[ self.played_chunk_number % self.cells_in_buffer ][1] = 16*self.number_of_channels
 
-    #Reduce own congestions exponentially
+    #Reproduce sound data
     self.play(outdata)
 
 if __name__ == "__main__":
