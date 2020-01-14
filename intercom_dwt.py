0e8f62a3ba85a225990b12535e757dc757039ae3
Gelrok <Toni.Schaarschmidt@hotmail.de>
2020-01-14 02:32:55 +0100

    issue DWT

--- intercom_dwt.py
+++ intercom_dwt.py
@@ -1,59 +1,3 @@
-# Using the Discrete Wavelet Transform, convert the chunks of samples
-# intro chunks of Wavelet coefficients (coeffs).
-#
-# The coefficients require more bitplanes than the original samples,
-# but most of the energy of the samples of the original chunk tends to
-# be into a small number of coefficients that are localized, usually
-# in the low-frequency subbands:
-#
-# (supposing a chunk with 1024 samples)
-#
-# Amplitude
-#     |       +                      *
-#     |   *     *                  *
-#     | *        *                *
-#     |*          *             *
-#     |             *       *
-#     |                 *
-#     +------------------------------- Time
-#     0                  ^        1023 
-#                |       |       
-#               DWT  Inverse DWT 
-#                |       |
-#                v
-# Amplitude
-#     |*
-#     |
-#     | *
-#     |  **
-#     |    ****
-#     |        *******
-#     |               *****************
-#     +++-+---+------+----------------+ Frequency
-#     0                            1023
-#     ^^ ^  ^     ^           ^
-#     || |  |     |           |
-#     || |  |     |           +--- Subband H1 (16N coeffs)
-#     || |  |     +--------------- Subband H2 (8N coeffs)
-#     || |  +--------------------- Subband H3 (4N coeffs)
-#     || +------------------------ Subband H4 (2N coeffs)
-#     |+-------------------------- Subband H5 (N coeffs)
-#     +--------------------------- Subband L5 (N coeffs)
-#
-# (each channel must be transformed independently)
-#
-# This means that the most-significant bitplanes, for most chunks
-# (this depends on the content of the chunk), should have only bits
-# different of 0 in the coeffs that belongs to the low-frequency
-# subbands. This will be exploited in a future issue.
-#
-# The straighforward implementation of this issue is to transform each
-# chun without considering the samples of adjacent
-# chunks. Unfortunately this produces an error in the computation of
-# the coeffs that are at the beginning and the end of each subband. To
-# compute these coeffs correctly, the samples of the adjacent chunks
-# i-1 and i+1 should be used when the chunk i is transformed:
-#
 #   chunk i-1     chunk i     chunk i+1
 # +------------+------------+------------+
 # |          OO|OOOOOOOOOOOO|OO          |
@@ -74,15 +18,11 @@ import numpy as np
 import math
 from intercom import Intercom
 from intercom_empty import Intercom_empty
+#from intercom_dfc import Intercom_DFC
 import pywt as wt
 
-skip = 16
-
-# Number of samples to synthesize
-number_of_samples = 512
-
 # Number of levels of the DWT
-levels = 4
+#levels = 4
 
 # Wavelet used
 wavelet = 'bior3.5'
@@ -90,40 +30,119 @@ wavelet = 'bior3.5'
 #padding = "symmetric"
 padding = "periodization"
 
+factor_precision = 1000
+
 if __debug__:
     import sys
 
 class Intercom_DWT(Intercom_empty):
+        def init(self, args):
+            Intercom_empty.init(self, args)
+            self.samples = 0
+            self.packet_format = f"!HBB{self.frames_per_chunk//8}B"
+            empty_channel=np.zeros(self.frames_per_chunk)
+            self.coeffs_empty = wt.wavedec(empty_channel, wavelet=wavelet, mode=padding)
+	    #self.coeffs_empty = wt.wavedec(empty_channel, wavelet=wavelet, level=levels, mode=padding)
+            arr_empty, self.slice_structure = wt.coeffs_to_array(self.coeffs_empty)
+            #self._buffer = [self.generate_zero_chunk()] * self.cells_in_buffer
+            self._buffer_coeffs = [None] * self.cells_in_buffer
+            self.max_NOBPTS=32*self.number_of_channels
+            self.NOBPTS=self.max_NOBPTS
+            self.NORB = self.max_NOBPTS
+            self.arr_temp= np.zeros((self.frames_per_chunk, self.number_of_channels), np.int32)
 
-    def init(self, args):
-        Intercom_empty.init(self, args)
-        self.samples = 0
+            for i in range(self.cells_in_buffer):
+                    self._buffer_coeffs[i] = self.generate_zero_arr()
 
-    def send(self, indata):
-            longitud = math.ceil(self.samples_per_chunk/self.number_of_channels/2)
-            #sys.stderr.write("\nLength: {}".format(longitud)); sys.stderr.flush()
+        def generate_zero_arr(self):
+            cell = np.zeros((self.frames_per_chunk, self.number_of_channels), np.int32)
+            return cell
 
-            canal_L=(indata[:,0]).flatten()
+        def receive_and_buffer(self):
+            message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+            received_chunk_number, received_bitplane_number, self.NORB, *bitplane = struct.unpack(self.packet_format, message)
+            bitplane = np.asarray(bitplane, dtype=np.uint8)
+            bitplane = np.unpackbits(bitplane)
+            bitplane = bitplane.astype(np.int32)
+            self._buffer_coeffs[received_chunk_number % self.cells_in_buffer][:, received_bitplane_number%self.number_of_channels] |= (bitplane << received_bitplane_number//self.number_of_channels)
+            self.received_bitplanes_per_chunk[received_chunk_number % self.cells_in_buffer] += 1
+            return received_chunk_number
+
+        def send_bitplane(self, arr_data, bitplane_number):
+            bitplane = (arr_data[:, bitplane_number%self.number_of_channels] >> bitplane_number//self.number_of_channels) & 1
+            bitplane = bitplane.astype(np.uint8)
+            bitplane = np.packbits(bitplane)
+            message = struct.pack(self.packet_format, self.recorded_chunk_number, bitplane_number, self.received_bitplanes_per_chunk[(self.played_chunk_number+1) % self.cells_in_buffer]+1, *bitplane)
+            self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+
+        def send(self, indata):
+            signs = indata & 0x8000
+            magnitudes = abs(indata)
+            indata = signs | magnitudes
             
-            for i in range(0,number_of_samples,skip):
-                coeffs=wt.wavedec(canal_L, wavelet=wavelet, level=levels, mode=padding)
-                
-                sys.stderr.write("\nCOEFFS: {}".format(coeffs[1])); sys.stderr.flush()
-            
-                arr, coeff_slices = wt.coeffs_to_array(coeffs)
-                arr[i]=number_of_samples
-                            
-                coeffs_from_arr = wt.array_to_coeffs(arr, coeff_slices, output_format="wavedec")
-                
-                
+            for ch in range (self.number_of_channels):
+                coeffs = wt.wavedec(indata[:,ch], wavelet=wavelet, mode=padding)
+                #coeffs = wt.wavedec(indata[:,ch], wavelet=wavelet, level=levels, mode=padding)
+                arr_float, slices = wt.coeffs_to_array(coeffs)
+                self.arr_temp[:,ch] = np.multiply(arr_float,factor_precision)
 
-                self.samples = wt.waverec(coeffs_from_arr, wavelet=wavelet, mode=padding)
-                
+            #sys.stderr.write("\nARR_S [{}]{} {} {} {}".format(self.recorded_chunk_number, self.arr_temp[0],self.arr_temp[1],self.arr_temp[2],self.arr_temp[3])); sys.stderr.flush()
+
+            #sys.stderr.write("\nCOE_S [{}]{}".format(self.recorded_chunk_number, coeffs[0])); sys.stderr.flush()
             
+            last_BPTS = self.max_NOBPTS - self.NOBPTS - 1
+            self.send_bitplane(self.arr_temp, self.max_NOBPTS-1)
+            self.send_bitplane(self.arr_temp, self.max_NOBPTS-2)
+
+            for bitplane_number in range(self.max_NOBPTS-3, last_BPTS, -1):
+                self.send_bitplane(self.arr_temp, bitplane_number)
+
+            self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+
+        def record_send_and_play_stereo(self, indata, outdata, frames, time, status):
+            indata[:,0] -= indata[:,1]
+
+            #sys.stderr.write("\nSEN_0 [{}]{}".format(self.recorded_chunk_number,indata[:,0])); sys.stderr.flush()
+            #sys.stderr.write("\nSEN_1 [{}]{}".format(self.recorded_chunk_number,indata[:,1])); sys.stderr.flush()
+            
+            self.send(indata)
+            arr = self._buffer_coeffs[self.played_chunk_number % self.cells_in_buffer]
+            
+
+            #sys.stderr.write("\nARR_R [{}]{} {} {} {}".format(self.played_chunk_number, arr[0],arr[1],arr[2],arr[3])); sys.stderr.flush()
+            
+            for ch in range (self.number_of_channels):
+                arr_chan = self._buffer_coeffs[self.played_chunk_number % self.cells_in_buffer][:,ch]
+                arr_chan = np.divide(arr_chan,factor_precision)
+                coeffs_chan = wt.array_to_coeffs(arr_chan, self.slice_structure, output_format="wavedec")
+                
+                recover = wt.waverec(coeffs_chan, wavelet=wavelet, mode=padding)
+
+                self._buffer[self.played_chunk_number % self.cells_in_buffer][:,ch]=recover.astype(np.int16)
+                #self._buffer[self.played_chunk_number % self.cells_in_buffer][:,ch]=np.asarray(recover,dtype=np.int16)
+
+
+            self._buffer_coeffs[self.played_chunk_number % self.cells_in_buffer]=self.generate_zero_arr()
+            chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+            signs = chunk >> 15
+            magnitudes = chunk & 0x7FFF
+            #chunk = ((~signs & magnitudes) | ((-magnitudes) & signs))
+            chunk = magnitudes + magnitudes*signs*2
+
+            #sys.stderr.write("\nREC_{} [{}]{}".format(0,self.played_chunk_number, chunk[:,0])); sys.stderr.flush()
+            #sys.stderr.write("\nREC_{} [{}]{}".format(1,self.played_chunk_number, chunk[:,1])); sys.stderr.flush()
+            
+            self._buffer[self.played_chunk_number % self.cells_in_buffer] = chunk
+            self._buffer[self.played_chunk_number % self.cells_in_buffer][:,0] += self._buffer[self.played_chunk_number % self.cells_in_buffer][:,1]
+            self.play(outdata)
+            self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+            self.received_bitplanes_per_chunk [self.played_chunk_number % self.cells_in_buffer] = 0
+
+
 
 if __name__ == "__main__":
-    intercom = Intercom_DWT()
-    parser = intercom.add_args()
-    args = parser.parse_args()
-    intercom.init(args)
-    intercom.run()
+	intercom = Intercom_DWT()
+	parser = intercom.add_args()
+	args = parser.parse_args()
+	intercom.init(args)
+	intercom.run()
